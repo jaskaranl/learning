00194d5f4c5a355869e64747068049e335b3f729
Jaskaran <jaskaran.kamboj@gameskraft.com>
2024-12-15 15:25:34 +0530

    d

--- zalgocontainer.js
+++ zalgocontainer.js
@@ -12,3 +12,30 @@ function zalgoContainer(cbTaker, cb) {
             cb(er, data);
     }
 }
+
+
+
+/// consistentReadAsync
+function consistentReadAsync (filename, callback) {
+    if (cache.has(filename)) {
+        // deferred callback invocation
+        process.nextTick(() => callback(cache.get(filename)))
+    } else {
+        // asynchronous function
+        readFile(filename, 'utf8', (err, data) => {
+            cache.set(filename, data)
+            callback(data)
+        })
+    }
+}
+
+///consistentReadSync
+function consistentReadSync (filename) {
+    if (cache.has(filename)) {
+        return cache.get(filename)
+    } else {
+        const data = readFileSync(filename, 'utf8')
+        cache.set(filename, data)
+        return data
+    }
+}
